a4094af237d3050e7394726731df849ff0823c93
Nils Fischer <fischern@bzz.ch>
2023-10-02 16:11:27 +0200

    __init.py implemented

--- src/visualizers/visualizer.py
+++ src/visualizers/visualizer.py
@@ -2,18 +2,17 @@ import tkinter as tk
 from tkinter import *
 
 
-
 class Visualizer:
     """
     Visualizer class
     """
+
     def __init__(self):
         """
         Constructor
         """
         root = tk.Tk()
         self.open_gui(root)
-
 
     def open_gui(self, root):
         """
@@ -37,13 +36,16 @@ class Visualizer:
         """
         header = tk.Canvas(root, width=1200, height=75, bg="#f9f9f9", highlightthickness=0)
         header.place(x=0, y=0)
-        export_btn = tk.Button(header, text="Exportieren", width=10, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9", borderwidth=0, font=("Arial", 13))
+        export_btn = tk.Button(header, text="Exportieren", width=10, bg="#cccccc", highlightthickness=0,
+                               highlightbackground="#e9e9e9", borderwidth=0, font=("Arial", 13))
         export_btn.place(relx=0.05, rely=0.5, anchor="center", height=50)
         date_text = tk.Label(header, text="Datum:", bg="#f9f9f9", font=("Arial", 20))
         date_text.place(relx=0.2, rely=0.5, anchor="center")
-        start_date_input = tk.Entry(header, width=10, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9", borderwidth=0, font=("Arial", 13), justify="center")
+        start_date_input = tk.Entry(header, width=10, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9",
+                                    borderwidth=0, font=("Arial", 13), justify="center")
         start_date_input.place(relx=0.28, rely=0.5, height=50, anchor="center")
-        end_date_input = tk.Entry(header, width=10, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9", borderwidth=0, font=("Arial", 13), justify="center")
+        end_date_input = tk.Entry(header, width=10, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9",
+                                  borderwidth=0, font=("Arial", 13), justify="center")
         end_date_input.place(relx=0.38, rely=0.5, height=50, anchor="center")
         legend_text = tk.Label(header, text="Legende:", bg="#f9f9f9", font=("Arial", 20))
         legend_text.place(relx=0.5, rely=0.5, anchor="center")
@@ -57,12 +59,9 @@ class Visualizer:
         red_text.place(relx=0.65, rely=0.7, anchor="center")
         options = ["Verbrauchsdiagramm", "Zählerstandsdiagramm"]
         dropdown = tk.OptionMenu(header, StringVar(), *options)
-        dropdown.config(width=20, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9", borderwidth=0, font=("Arial", 13))
+        dropdown.config(width=20, bg="#cccccc", highlightthickness=0, highlightbackground="#e9e9e9", borderwidth=0,
+                        font=("Arial", 13))
         dropdown.place(relx=0.85, rely=0.5, anchor="center", height=50)
-
-
-
-
 
     def body(self, root):
         """
@@ -70,10 +69,6 @@ class Visualizer:
         """
         body = tk.Canvas(root, width=1200, height=600, bg="#ffffff", highlightthickness=0)
         body.place(x=0, y=75)
-
-
-
-
 
 
 if __name__ == "__main__":
