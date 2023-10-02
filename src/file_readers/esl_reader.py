1c851509408b64116d043d5c6e62f8676a7b59f0
Restyled.io <commits@restyled.io>
2023-10-02 11:10:28 +0000

    Restyled by black

--- src/file_readers/esl_reader.py
+++ src/file_readers/esl_reader.py
@@ -7,13 +7,13 @@ class ESLReader:
         self.root = self.tree.getroot()
 
     def get_interval(self):
-        startTime = self.root.find('.//TimePeriod').attrib['end']
-        endTime = self.root.find('.//Header').attrib['created']
+        startTime = self.root.find(".//TimePeriod").attrib["end"]
+        endTime = self.root.find(".//Header").attrib["created"]
         return f"Starting Time:\n{startTime}\nEnd Time:\n{endTime}"
 
     def get_values(self):
         values = []
-        bunch = self.root.find('.//TimePeriod')
+        bunch = self.root.find(".//TimePeriod")
         for child in bunch:
             if child.attrib["obis"] == "1-1:1.8.1":
                 values.append(child.attrib["value"])
@@ -29,7 +29,8 @@ class ESLReader:
 
 if __name__ == "__main__":
     esl = ESLReader(
-        "data\ESL-Files\EdmRegisterWertExport_20190131_eslevu_20190322160349.xml")
+        "data\ESL-Files\EdmRegisterWertExport_20190131_eslevu_20190322160349.xml"
+    )
     print("-----------------------")
     print(esl.get_interval())
     print("-----------------------")
