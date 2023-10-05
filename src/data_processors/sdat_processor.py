160c2dd20b2defa403d3c24c1b9e8c01206aedf4
Ferni <meierf@bzz.ch>
2023-10-05 22:25:49 +0200

    added: favicon, languages and settings.json generator

--- src/data_processors/sdat_processor.py
+++ src/data_processors/sdat_processor.py
@@ -1,22 +1,27 @@
+from collections import defaultdict
+import numpy as np
 import os
 import pandas as pd
-import numpy as np
 from datetime import datetime, timedelta
+import sys
+dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(dir))
+
+# Assume this import works as in your original code
 from file_readers.sdat_reader import SDATReader
 
 class SDATProcessor:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.data = pd.DataFrame()
+        self.data_list = []
 
     def process_files(self):
-        # Liste aller SDAT-Dateien im Ordner
         files = [f for f in os.listdir(self.folder_path) if f.endswith('.xml')]
-        
-        for file in files:
+        total_files = len(files)
+
+        for file_index, file in enumerate(files):
             file_path = os.path.join(self.folder_path, file)
             
-            # Verwenden Sie SDATReader, um die Daten zu lesen
             reader = SDATReader(file_path)
             document_id = reader.get_document_id()
             start_time, end_time = reader.get_interval()
@@ -24,42 +29,72 @@ class SDATProcessor:
             resolution, unit = reader.get_resolution()
             resolution = int(resolution)
             observations = reader.get_observations()
-            
-            # Generieren Sie Zeitstempel und teilen Sie die Beobachtungen in Verbrauch und Produktion auf
-            timestamps = []
-            consumption = []
-            production = []
-            
+
             for i, (seq, vol) in enumerate(observations):
                 current_time = start_time + timedelta(minutes=i * resolution)
-                timestamps.append(current_time)
-                
                 vol = float(vol)
-                
+
+                data_dict = {
+                    'Timestamp': current_time,
+                    'Consumption': None,
+                    'Production': None
+                }
+
                 if 'ID735' in document_id:
-                    consumption.append(None)
-                    production.append(vol)
+                    data_dict['Production'] = vol
                 elif 'ID742' in document_id:
-                    consumption.append(vol)
-                    production.append(None)
+                    data_dict['Consumption'] = vol
                 else:
                     continue
-            
-            # Erstellen Sie einen DataFrame für diese Datei
-            df = pd.DataFrame({
-                'Timestamp': timestamps,
-                'Consumption': consumption,
-                'Production': production
-            })
-            df.set_index('Timestamp', inplace=True)
-            
-            self.data = pd.concat([self.data, df])
+                
+                self.data_list.append(data_dict)
+
+            # self.print_progress_bar(file_index + 1, total_files, prefix='Reading SDAT:', suffix='Complete', length=50)
+        
+        # Aggregating data by Timestamp
+        aggregated_data = defaultdict(lambda: {'Consumption': [], 'Production': []})
+        for data_dict in self.data_list:
+            timestamp = data_dict['Timestamp']
+            aggregated_data[timestamp]['Consumption'].append(data_dict['Consumption'])
+            aggregated_data[timestamp]['Production'].append(data_dict['Production'])
+
+        # Processing the aggregated data
+        processed_data_list = []
+        for timestamp, data in aggregated_data.items():
+            consumption_values = [x for x in data['Consumption'] if x is not None]
+            production_values = [x for x in data['Production'] if x is not None]
+
+            processed_data_dict = {
+                'Timestamp': timestamp,
+                'Consumption': self.process_value_list(consumption_values),
+                'Production': self.process_value_list(production_values)
+            }
+            processed_data_list.append(processed_data_dict)
+
+        self.data = pd.DataFrame(processed_data_list)
+        self.data.set_index('Timestamp', inplace=True)
+        self.data.sort_index(inplace=True)
 
     def get_data_for_plotting(self):
         return self.data
 
+    def process_value_list(self, values):
+        if not values:
+            return 0
+        non_zero_values = [x for x in values if x != 0]
+        if not non_zero_values:
+            return 0
+        return np.mean(non_zero_values)  # Take average of all non-zero numbers
+
+    def print_progress_bar(self, iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '-' * (length - filled_length)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
+
 if __name__ == '__main__':
-    processor = SDATProcessor('./data/sdat_files/')
+    processor = SDATProcessor('./data/SDAT-Files')
     processor.process_files()
     plot_data = processor.get_data_for_plotting()
     print(plot_data)
+    plot_data.to_csv('./output/debug/final_output.csv')
