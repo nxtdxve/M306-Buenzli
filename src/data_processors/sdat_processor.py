d50a64a63e361a30e58ecc3dc6013b6e09d86b0d
nxtdxve <zettlerd@bzz.ch>
2023-10-05 13:34:28 +0200

    fixxed sdat_rpocessor

--- src/data_processors/sdat_processor.py
+++ src/data_processors/sdat_processor.py
@@ -1,9 +1,13 @@
+from collections import defaultdict
+import numpy as np
 import os
 import pandas as pd
 from datetime import datetime, timedelta
 import sys
 dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(dir))
+
+# Assume this import works as in your original code
 from file_readers.sdat_reader import SDATReader
 
 class SDATProcessor:
@@ -47,15 +51,40 @@ class SDATProcessor:
 
             self.print_progress_bar(file_index + 1, total_files, prefix='Reading SDAT:', suffix='Complete', length=50)
         
-        # Erstellen Sie den DataFrame nachdem alle Dateien verarbeitet wurden
-        self.data = pd.DataFrame(self.data_list)
+        # Aggregating data by Timestamp
+        aggregated_data = defaultdict(lambda: {'Consumption': [], 'Production': []})
+        for data_dict in self.data_list:
+            timestamp = data_dict['Timestamp']
+            aggregated_data[timestamp]['Consumption'].append(data_dict['Consumption'])
+            aggregated_data[timestamp]['Production'].append(data_dict['Production'])
+
+        # Processing the aggregated data
+        processed_data_list = []
+        for timestamp, data in aggregated_data.items():
+            consumption_values = [x for x in data['Consumption'] if x is not None]
+            production_values = [x for x in data['Production'] if x is not None]
+
+            processed_data_dict = {
+                'Timestamp': timestamp,
+                'Consumption': self.process_value_list(consumption_values),
+                'Production': self.process_value_list(production_values)
+            }
+            processed_data_list.append(processed_data_dict)
+
+        self.data = pd.DataFrame(processed_data_list)
         self.data.set_index('Timestamp', inplace=True)
+        self.data.sort_index(inplace=True)
 
     def get_data_for_plotting(self):
-        # Konvertieren Sie die Datenliste direkt hier in einen DataFrame
-        plot_data = pd.DataFrame(self.data_list)
-        plot_data.set_index('Timestamp', inplace=True)
-        return plot_data
+        return self.data
+
+    def process_value_list(self, values):
+        if not values:
+            return 0
+        non_zero_values = [x for x in values if x != 0]
+        if not non_zero_values:
+            return 0
+        return np.mean(non_zero_values)  # Take average of all non-zero numbers
 
     def print_progress_bar(self, iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
         percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
